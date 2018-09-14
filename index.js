5281cfabc21c3592181f552e1d10631a8b1adb66
Pranay Ankit <pranayankit414@gmail.com>
2018-09-15 02:25:24 +0530

    feature added: cloning

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const chalk = require('chalk');
 const figlet = require('figlet');
 const clear = require('clear');
+const fs = require('fs');
+const path = require('path');
 const CLI = require('clui');
 const Spinner = CLI.Spinner;
 
@@ -8,7 +10,8 @@ const inquirer = require('./lib/inquirer');
 const github = require('./lib/github');
 const repo = require('./lib/repo');
 
-const status = new Spinner('Loading repos, please wait..')
+const status = new Spinner('Loading repos, please wait..');
+const repo_path = 'https://github.com/pranay414/github-scaffold.git';
 
 // Clear the screen and
 // display banner text 'Git CleanUp'
@@ -20,15 +23,15 @@ console.log(
 
 // Ask for user's Github credentials
 const run = async () => {
-    let token = github.getStoredGithubToken();
+    let token = github.getStoredGithubToken()
 
     if (!token) {
-        await github.setGithubCredentials();
-        token = await github.regsiterNewToken();
+        await github.setGithubCredentials()
+        token = await github.regsiterNewToken()
     }
 
     status.start();
-    let userResponse = await repo.getPrivateRepos(token);
+    let userResponse = await repo.getPrivateRepos(token)
 
     if (userResponse === null) {
         console.log(chalk.red(`No private repos found!`))
@@ -37,7 +40,17 @@ const run = async () => {
         status.stop();
         let action = await inquirer.askUserActions();
         if (action.option === 'Clone and delete all the private repos') {
-            console.log(userResponse)
+            // Check if directory `cloned` exists or not
+            if (!fs.existsSync('cloned')) {
+                console.log(chalk.blue('Creating directory ./cloned'))
+                fs.mkdirSync('cloned')
+            }
+            console.log(chalk.blue('Cloning repository...'))
+
+            // Clone and delete every private repository on Github
+            for(repo in userResponse) {
+
+            }
         }
     }
 }
