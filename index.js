125561b07d3c6f0672345d77a82019c3e1bf33ce
Pranay Ankit <pranayankit414@gmail.com>
2018-09-16 10:59:02 +0530

    added feature to clone repos

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const chalk = require('chalk');
 const figlet = require('figlet');
 const clear = require('clear');
-const fs = require('fs');
-const path = require('path');
 const CLI = require('clui');
 const Spinner = CLI.Spinner;
 
@@ -11,7 +9,6 @@ const github = require('./lib/github');
 const repo = require('./lib/repo');
 
 const status = new Spinner('Loading repos, please wait..');
-const repo_path = 'https://github.com/pranay414/github-scaffold.git';
 
 // Clear the screen and
 // display banner text 'Git CleanUp'
@@ -40,17 +37,7 @@ const run = async () => {
         status.stop();
         let action = await inquirer.askUserActions();
         if (action.option === 'Clone and delete all the private repos') {
-            // Check if directory `cloned` exists or not
-            if (!fs.existsSync('cloned')) {
-                console.log(chalk.blue('Creating directory ./cloned'))
-                fs.mkdirSync('cloned')
-            }
-            console.log(chalk.blue('Cloning repository...'))
-
-            // Clone and delete every private repository on Github
-            for(repo in userResponse) {
-
-            }
+            repo.cloneAndDelete(userResponse);
         }
     }
 }
